44d14b0026ec25cec1fc928996a4963023ab7bda
Gus Bikos <gusbikos@gmail.com>
2021-01-22 06:49:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,15 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+
+  emotes = YAML.load_file('emoticons.yml')
+  puts emotes.inspect
+
+
+
 end
+
 
 def get_japanese_emoticon
   # code goes here
